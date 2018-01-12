513ea5ed2622fb6003e1a02ba03e4e18f408cae8
Farah Abdi <farah.abdi@isobar.com>
2018-01-12 13:20:18 +1100

    Add starting stylesheets.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAuth } from '../reducers/selectors';
 import { AuthenticatedPage } from './AuthenticatedPage';
 import { PublicPage } from './PublicPage';
 
-const App = () => {
-  const { authenticated } = this.props;
-  return (
-    <main>
-      {authenticated ? (
-        <AuthenticatedPage />
+export class App extends Component {
+  componentWillMount() { }
+
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <main>
+        {authenticated ? (
+          <AuthenticatedPage />
           ) : (
             <PublicPage />
         )}
-    </main>
-  );
+      </main>
+    );
+  }
+}
+
+App.propTypes = {
+  authenticated: PropTypes.func.isRequired,
 };
-
-
 const mapStateToProps = getAuth;
 
 export default connect(mapStateToProps, null)(App);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { initAuth } from './actions/authActions';
-import App from './containers/App';
+import { App } from './containers/App';
+import './styles/index.scss';
 
 const store = configureStore();
 
