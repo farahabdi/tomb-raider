04636f8243a350a481d36e2642f02072fd597cdf
Farah Abdi <farah.abdi@isobar.com>
2018-02-06 13:25:39 +1100

    quick fix

--- src/leaflet/initApp.js
+++ src/leaflet/initApp.js
@@ -30,9 +30,9 @@ export default async function initApp() {
   searchControl();
   tabsControl();
 
-  // let visited = await fetchUserVisited()
+  let visited = await fetchUserVisited()
 
-  let visited = true
+  //let visited = true
 
   let imagesURLs = getMapImageURLs();
   loadImages(imagesURLs, () => {
